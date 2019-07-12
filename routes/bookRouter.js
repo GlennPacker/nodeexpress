cd959aedda4d520f05b553333be6db1316c819e5
DESKTOP-ACT9QHL\Glenn <glenn@glennpacker.net>
2019-07-12 15:43:36 +0100

    book put, patch and delete

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -21,11 +21,56 @@ function routes(Book) {
       });
     });
 
-  bookRouter.route("/books/:bookId").get((req, res) => {
+  bookRouter.use("/books/:bookId", (req, res, next) => {
     Book.findById(req.params.bookId, (err, book) => {
-      return err ? res.send(err) : res.json(book);
+      if (err) {
+        return res.send(err);
+      }
+      if (!book) {
+        return res.sendStatus(404);
+      }
+      req.book = book;
+      return next();
     });
   });
+  bookRouter
+    .route("/books/:bookId")
+    .get((req, res) => {
+      res.json(req.book);
+    })
+    .put((req, res) => {
+      const { book } = req;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.genre = req.body.genre;
+      book.read = req.body.read;
+
+      book.save(err => {
+        if (err) return res.send(err);
+      });
+
+      return res.json(book);
+    })
+    .patch((req, res) => {
+      const { book } = req;
+
+      Object.entries(req.body).forEach(item => {
+        if (item[0] !== "_id") {
+          book[item[0]] = item[1];
+        }
+      });
+
+      book.save(err => {
+        if (err) return res.send(err);
+        return res.json(book);
+      });
+    })
+    .delete((req, res) => {
+      req.book.remove(err => {
+        if (err) return res.send(err);
+        return res.sendStatus(204);
+      });
+    });
 
   return bookRouter;
 }
